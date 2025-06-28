01314dbe13f34cf8b1032633c1aa3857c35e6ce7
pauljako <paulifey18@proton.me>
2025-06-29 00:25:22 +0200

    hacky cookie parsing

--- js/devtest.mjs
+++ js/devtest.mjs
@@ -1,25 +1,24 @@
+var cookieObject = JSON.parse(atob(document.cookie.replace("data=", "")));
 
-var cookieObject = JSON.parse(document.cookie);
-
-console.log(cookieObject)
-
+console.log(cookieObject);
 
 var requestOptions = {
-    method: 'GET',
-}
+  method: "GET",
+  headers: cookieObject["headers"],
+};
 
-console.log(requestOptions)
+console.log(requestOptions);
 
 fetch("https://impostors.api.pauljako.de/api/v1/post/random", requestOptions)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
   });
