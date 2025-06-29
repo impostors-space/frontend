8fffd9971da456cc29ce35b5b9bb9baec9a57dd2
pauljako <paulifey18@proton.me>
2025-06-29 12:26:14 +0200

    temporarly fixed login cookie

--- js/login.js
+++ js/login.js
@@ -40,9 +40,9 @@ async function login(uname, psw) {
     requestOptions,
   )
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
+      // if (!response.ok) {
+      //  throw new Error("Network response was not ok");
+      // }
       return response.json();
     })
     .then((data) => {
