6ff1cffe39403aceb54f5600a0795f23b0db0815
Paul Fey <paulifey18@proton.me>
2025-06-30 20:12:13 +0200

    fix(js/createpost.js): post creation not being responsive

--- js/createpost.js
+++ js/createpost.js
@@ -1,35 +1,30 @@
-
 var cookieObject = JSON.parse(atob(document.cookie.replace("data=", "")));
 
 async function createPost(content) {
-
-    await fetch(
-        `https://impostors.api.pauljako.de/api/v1/post`,
-        {
-            method: "POST",
-            headers: cookieObject["headers"],
-            body: content,
-        },
-    )
+  await fetch(`https://impostors.api.pauljako.de/api/v1/post`, {
+    method: "POST",
+    headers: cookieObject["headers"],
+    body: content,
+  })
     .then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
     .then((data) => {
-        reloadPost();
+      location.href = "/post.html";
 
-        console.log(data);
+      console.log(data);
     })
     .catch((error) => {
-        console.error("Error:", error);
+      console.error("Error:", error);
     });
 }
 
-var contentField = document.getElementById("content-field")
-var submitButton = document.getElementById("submit-button")
+var contentField = document.getElementById("content-field");
+var submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", function () {
-    createPost(contentField.value)
-})
+  createPost(contentField.value);
+});
